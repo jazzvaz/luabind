99ef62ae38532c1af38491ea62385f24d9fd2ecc
Pavel Kovalenko <su@nitrocaster.me>
2020-04-19 20:00:06 +0300

    Fix lua_raw{get,set}p functions for Lua 5.1. Previous implementations of these functions caused LuaJIT to crash with   lj_api.c:49: index2adr: Assertion `fn->c.gct == ~(~8u) && !((fn)->c.ffid== 0)' failed.
    Use the implementation by Hisham Muhammad (see https://github.com/hishamhm/lua-compat-5.2)
    
    Credits to @the-nic

--- luabind/lua_include.hpp
+++ luabind/lua_include.hpp
@@ -44,21 +44,27 @@ extern "C"
 # define lua_getuservalue lua_getfenv
 # define lua_setuservalue lua_setfenv
 
-inline void lua_rawsetp(lua_State* L, int t, void* k)
+inline int lua_absindex(lua_State* L, int i)
 {
-	lua_pushlightuserdata(L, k);
-	lua_insert(L, -2); // Move key beneath value.
-	if (t < 0)
-		t -= 1; // Adjust for pushed k.
-	lua_rawset(L, t);
+	if (i < 0 && i > LUA_REGISTRYINDEX)
+		i += lua_gettop(L) + 1;
+	return i;
 }
 
-inline void lua_rawgetp(lua_State* L, int t, void* k)
+inline void lua_rawsetp(lua_State* L, int i, void* p)
 {
-	lua_pushlightuserdata(L, k);
-	if (t < 0)
-		t -= 1; // Adjust for pushed k.
-	lua_rawget(L, t);
+	int abs_i = lua_absindex(L, i);
+	luaL_checkstack(L, 1, "not enough stack slots");
+	lua_pushlightuserdata(L, p);
+	lua_insert(L, -2);
+	lua_rawset(L, abs_i);
+}
+
+inline void lua_rawgetp(lua_State* L, int i, void* p)
+{
+	int abs_i = lua_absindex(L, i);
+	lua_pushlightuserdata(L, p);
+	lua_rawget(L, abs_i);
 }
 #endif
 
