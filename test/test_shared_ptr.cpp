30a05f10da26c8dfadc53214b226ddd2aabae8c4
Pavel Kovalenko <su@nitrocaster.me>
2020-05-03 01:14:27 +0300

    Add additional checks to shared_ptr test. This tests the issue reported here: https://github.com/Oberon00/luabind/issues/4
    Credits to @Oberon00

--- test/test_shared_ptr.cpp
+++ test/test_shared_ptr.cpp
@@ -10,10 +10,20 @@ struct X
 {
     X(int value)
       : value(value)
-    {}
+    {
+		alive++;
+	}
+
+	~X()
+	{
+		alive--;
+	}
 
     int value;
+	static int alive;
 };
+
+int X::alive = 0;
 
 int get_value(std::shared_ptr<X> const& p)
 {
@@ -25,6 +35,11 @@ std::shared_ptr<X> filter(std::shared_ptr<X> const& p)
     return p;
 }
 
+std::shared_ptr<X> make_x()
+{
+	return std::shared_ptr<X>(luabind::luabind_new<X>(0), luabind::luabind_delete<X>);
+}
+
 void test_main(lua_State* L)
 {
     using namespace luabind;
@@ -33,7 +48,8 @@ void test_main(lua_State* L)
         class_<X>("X")
             .def(constructor<int>()),
         def("get_value", &get_value),
-        def("filter", &filter)
+        def("filter", &filter),
+		def("make_x", &make_x)
     ];
 
     DOSTRING(L,
@@ -43,6 +59,25 @@ void test_main(lua_State* L)
 
     DOSTRING(L,
         "assert(x == filter(x))\n"
+		"x = nil\n"
+		"collectgarbage()\n"
     );
+	DOSTRING(L,
+		"sx = make_x()\n"
+		"function get_sx() return sx end"
+	);
+	std::shared_ptr<X> spx = call_function<std::shared_ptr<X>>(L, "get_sx");
+	TEST_CHECK(spx.use_count() == 2);
+	DOSTRING(L,
+		"sx = nil\n"
+		"collectgarbage()\n"
+	);
+	TEST_CHECK(spx.use_count() == 1);
+	TEST_CHECK(X::alive == 1);
+	spx.reset(); // reference to lua object is released here
+	DOSTRING(L,
+		"collectgarbage()\n" // unreferenced object is collected
+	);
+	TEST_CHECK(X::alive == 0);
 }
 
