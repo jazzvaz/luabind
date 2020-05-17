db85ca7d169a4ca9950265e3d14b5344fc77b838
Pavel Kovalenko <su@nitrocaster.me>
2020-05-17 16:22:41 +0300

    type_traits: Refactor with helper templates.

--- luabind/detail/type_traits.hpp
+++ luabind/detail/type_traits.hpp
@@ -10,30 +10,22 @@
 namespace luabind {
 
 	namespace detail {
+		
+		template<typename T>
+		using is_const_reference =
+			std::bool_constant<std::is_reference_v<T> && std::is_const_v<std::remove_reference_t<T>>>;
 
-		template< typename T >
-		struct is_const_reference
-			: public std::conditional< std::is_reference<T>::value && std::is_const<typename std::remove_reference<T>::type>::value, std::true_type, std::false_type >::type
-		{
-		};
+		template<typename T>
+		using is_nonconst_reference =
+			std::bool_constant<std::is_reference_v<T> && !std::is_const_v<std::remove_reference_t<T>>>;
 
-		template<class T>
-		struct is_nonconst_reference
-			: public std::conditional< std::is_reference<T>::value && !std::is_const<typename std::remove_reference<T>::type>::value, std::true_type, std::false_type >::type
-		{
-		};
+		template<typename T>
+		using is_const_pointer =
+			std::bool_constant<std::is_pointer_v<T> && std::is_const_v<std::remove_pointer_t<T>>>;
 
-		template<class T>
-		struct is_const_pointer
-			: public std::conditional< std::is_const<typename std::remove_pointer<T>::type>::value && std::is_pointer<T>::value, std::true_type, std::false_type >::type
-		{
-		};
-
-		template<class T>
-		struct is_nonconst_pointer :
-			public std::conditional < std::is_pointer<T>::value && !std::is_const<typename std::remove_pointer<T>::type>::value, std::true_type, std::false_type >::type
-		{
-		};
+		template<typename T>
+		using is_nonconst_pointer =
+			std::bool_constant<std::is_pointer_v<T> && !std::is_const_v<std::remove_pointer_t<T>>>;
 
 		template<int v1, int v2>
 		struct max_c
@@ -44,29 +36,10 @@ namespace luabind {
 	} // namespace detail
 
 	template< typename T >
-	struct remove_const_reference {
-		using type = typename std::remove_const<typename std::remove_reference<T>::type>::type;
-	};
-
-	template< typename T >
-	using remove_const_reference_t = typename remove_const_reference<T>::type;
-
-	//
-	// most_derived
-	//
-
-	template<class A, class B>
-	struct most_derived
-	{
-		using type = typename std::conditional<
-			std::is_base_of<A, B>::value
-			, B
-			, A
-		>::type;
-	};
+	using remove_const_reference_t = std::remove_const_t<std::remove_reference_t<T>>;
 
 	template< typename A, typename B >
-	using most_derived_t = typename most_derived<A, B>::type;
+	using most_derived_t = std::conditional_t<std::is_base_of_v<A, B>, B, A>;
 
 	//
 	// null_type
@@ -84,7 +57,8 @@ namespace luabind {
 	// deduce_signature
 	//
 
-	template< typename, typename > struct tagged_function;
+	template< typename, typename >
+	struct tagged_function;
 
 	template< typename T, typename WrappedType = null_type, typename = void >
 	struct deduce_signature;
@@ -116,13 +90,13 @@ namespace luabind {
 	template< typename R, typename Class, typename... Args, class WrappedType >
 	struct deduce_signature < R(Class::*)(Args...), WrappedType >
 	{
-		using type = meta::type_list< R, typename most_derived<Class, WrappedType>::type&, Args... >;
+		using type = meta::type_list< R, most_derived_t<Class, WrappedType>&, Args... >;
 	};
 
 	template< typename R, typename Class, typename... Args, class WrappedType >
 	struct deduce_signature < R(Class::*)(Args...) const, WrappedType >
 	{
-		using type = meta::type_list< R, typename most_derived<Class, WrappedType>::type const&, Args... >;
+		using type = meta::type_list< R, most_derived_t<Class, WrappedType> const&, Args... >;
 	};
 
 	template< typename Signature, typename F, class WrappedType >
@@ -158,15 +132,19 @@ namespace luabind {
 	// is_reference_wrapper
 	//
 
-	template< typename T > struct is_reference_wrapper : public std::false_type { };
-	template< typename T > struct is_reference_wrapper< std::reference_wrapper<T> > : public std::true_type { };
+	template< typename T >
+	struct is_reference_wrapper : public std::false_type { };
+	template< typename T >
+	struct is_reference_wrapper< std::reference_wrapper<T> > : public std::true_type { };
 
 	//
 	// apply_reference_wrapper
 	//
 
-	template< typename T > struct apply_reference_wrapper { using type = T; };
-	template< typename T > struct apply_reference_wrapper< std::reference_wrapper<T> > { using type = T&; };
+	template< typename T >
+	struct apply_reference_wrapper { using type = T; };
+	template< typename T >
+	struct apply_reference_wrapper< std::reference_wrapper<T> > { using type = T&; };
 
 	template< typename T >
 	using apply_reference_wrapper_t = typename apply_reference_wrapper<T>::type;
@@ -175,11 +153,13 @@ namespace luabind {
 	// identity
 	//
 
-	template< typename T > struct identity { using type = T; };
+	template< typename T >
+	struct identity { using type = T; };
 
 	template< typename T >
 	using identity_t = typename identity<T>::type;
 
-	template< typename Dst > Dst implicit_cast(typename identity<Dst>::type t) { return t; }
+	template< typename Dst >
+	Dst implicit_cast(identity_t<Dst> t) { return t; }
 
 } // namespace luabind
