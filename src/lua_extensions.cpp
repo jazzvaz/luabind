6f897ee56837fcec3c5d37eb7e082fb73c154208
Pavel Kovalenko <su@nitrocaster.me>
2020-05-20 01:16:56 +0300

    luaL_tolstring_diag: ignore __tostring metamethod.
    This restores expected type names in function signatures and fixes tests
    which compare signature strings.

--- src/lua_extensions.cpp
+++ src/lua_extensions.cpp
@@ -40,42 +40,39 @@ namespace luabind
 	{
 		const char* luaL_tolstring_diag(lua_State* L, int idx, size_t* len)
 		{
-			if (!luaL_callmeta(L, idx, "__tostring"))
+			int t = lua_type(L, idx);
+			switch (t)
 			{
-				int t = lua_type(L, idx);
-				switch (t)
-				{
-				case LUA_TNIL:
-					lua_pushliteral(L, "nil");
-					break;
-				case LUA_TSTRING:
-					lua_pushliteral(L, "'");
-					lua_pushvalue(L, idx);
-					lua_pushliteral(L, "'");
-					lua_concat(L, 3);
-					break;
-				case LUA_TNUMBER:
-					lua_pushvalue(L, idx);
-					break;
-				case LUA_TBOOLEAN:
-					if (lua_toboolean(L, idx))
-						lua_pushliteral(L, "true");
-					else
-						lua_pushliteral(L, "false");
-					break;
-				case LUA_TUSERDATA:
-				{
-					// Print luabind::object class name
-					auto obj = argument(from_stack(L, idx));
-					lua_pushstring(L, get_class_info(obj).name.c_str());
-					lua_pushliteral(L, "&");
-					lua_concat(L, 2);
-					break;
-				}
-				default:
-					lua_pushfstring(L, "%s: %p", lua_typename(L, t), lua_topointer(L, idx));
-					break;
-				}
+			case LUA_TNIL:
+				lua_pushliteral(L, "nil");
+				break;
+			case LUA_TSTRING:
+				lua_pushliteral(L, "'");
+				lua_pushvalue(L, idx);
+				lua_pushliteral(L, "'");
+				lua_concat(L, 3);
+				break;
+			case LUA_TNUMBER:
+				lua_pushvalue(L, idx);
+				break;
+			case LUA_TBOOLEAN:
+				if (lua_toboolean(L, idx))
+					lua_pushliteral(L, "true");
+				else
+					lua_pushliteral(L, "false");
+				break;
+			case LUA_TUSERDATA:
+			{
+				// Print luabind::object class name
+				auto obj = argument(from_stack(L, idx));
+				lua_pushstring(L, get_class_info(obj).name.c_str());
+				lua_pushliteral(L, "&");
+				lua_concat(L, 2);
+				break;
+			}
+			default:
+				lua_pushfstring(L, "%s: %p", lua_typename(L, t), lua_topointer(L, idx));
+				break;
 			}
 			return lua_tolstring(L, -1, len);
 		}
