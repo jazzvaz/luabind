1b15e0b392ae7685db4cf3b62154fb50bc9d9d6d
Pavel Kovalenko <su@nitrocaster.me>
2020-05-24 19:17:11 +0300

    primitives.hpp: Refactor & reformat.

--- luabind/detail/primitives.hpp
+++ luabind/detail/primitives.hpp
@@ -7,33 +7,30 @@
 #include <type_traits>  // std::true_type...
 #include <cstring>
 
-namespace luabind {
-	namespace detail {
+namespace luabind::detail
+{
+	template <class T>
+	struct type_ {};
 
-		template<class T>
-		struct type_ {};
+	struct ltstr
+	{
+		bool operator()(char const* s1, char const* s2) const
+		{ return std::strcmp(s1, s2) < 0; }
+	};
 
-		struct ltstr
-		{
-			bool operator()(const char* s1, const char* s2) const { return std::strcmp(s1, s2) < 0; }
-		};
+	template <int N>
+	struct aligned
+	{
+		char storage[N];
+	};
 
-		template<int N>
-		struct aligned
-		{
-			char storage[N];
-		};
-
-		// returns the offset added to a Derived* when cast to a Base*
-		template<class Derived, class Base>
-		ptrdiff_t ptr_offset(type_<Derived>, type_<Base>)
-		{
-			aligned<sizeof(Derived)> obj;
-			Derived* ptr = reinterpret_cast<Derived*>(&obj);
-
-			return ptrdiff_t(static_cast<char*>(static_cast<void*>(static_cast<Base*>(ptr)))
-				- static_cast<char*>(static_cast<void*>(ptr)));
-		}
-
+	// returns the offset added to a Derived* when cast to a Base*
+	template <class Derived, class Base>
+	ptrdiff_t ptr_offset(type_<Derived>, type_<Base>)
+	{
+		aligned<sizeof(Derived)> obj;
+		auto* derived = reinterpret_cast<Derived*>(&obj);
+		auto* base = static_cast<Base*>(derived);
+		return ptrdiff_t(reinterpret_cast<char*>(base) - reinterpret_cast<char*>(derived));
 	}
-}
+} // namespace luabind::detail
