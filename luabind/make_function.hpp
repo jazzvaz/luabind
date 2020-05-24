b5c00afb6865ba7309e01f123c0bc0bb95381164
Pavel Kovalenko <su@nitrocaster.me>
2020-05-24 19:32:04 +0300

    make_function.hpp: Refactor & reformat.

--- luabind/make_function.hpp
+++ luabind/make_function.hpp
@@ -11,100 +11,103 @@
 #include <luabind/detail/format_signature.hpp>
 #include <tuple>
 
-namespace luabind {
-
-	namespace detail
-	{
-# ifndef LUABIND_NO_EXCEPTIONS
-		LUABIND_API void handle_exception_aux(lua_State* L);
-# endif
-
-		// MSVC complains about member being sensitive to alignment (C4121)
-		// when F is a pointer to member of a class with virtual bases.
-# ifdef _MSC_VER
-#  pragma pack(push)
-#  pragma pack(16)
-# endif
-
-		template <class F, class Signature, class InjectorList>
-		struct function_object_impl : function_object
-		{
-			function_object_impl(F f)
-				: function_object(&entry_point), f(f)
-			{}
-
-			int call(lua_State* L, invoke_context& ctx, int args) /*const*/
-			{
-				return invoke_best_match<InjectorList, Signature>(L, *this, ctx, f, args);
-			}
-
-			int format_signature(lua_State* L, char const* function, bool concat = true) const
-			{
-				return detail::format_signature(L, function, Signature(), concat);
-			}
-
-			static std::tuple<bool, int> safe_entry_point(lua_State* L)
-			{
-				void* ud = lua_touserdata(L, lua_upvalueindex(1));
-				function_object_impl* impl = *static_cast<function_object_impl**>(ud);
-				int results = 0;
-				bool error = false;
+namespace luabind::detail
+{
 #ifndef LUABIND_NO_EXCEPTIONS
-				try
+	LUABIND_API void handle_exception_aux(lua_State* L);
 #endif
-				{
-					invoke_context ctx;
-					results = invoke<InjectorList, Signature>(L, *impl, ctx, impl->f);
-					if (!get_permissive_mode() && !ctx)
-					{
-						error = true;
-						ctx.format_error(L, impl);
-					}
-				}
+	// MSVC complains about member being sensitive to alignment (C4121)
+	// when F is a pointer to member of a class with virtual bases.
+#ifdef _MSC_VER
+# pragma pack(push)
+# pragma pack(16)
+#endif
+	template <class F, class ArgList, class PolicyList>
+	struct function_object_impl : function_object
+	{
+		function_object_impl(F f) :
+			function_object(&entry_point),
+			f(f)
+		{}
+
+		int call(lua_State* L, invoke_context& ctx, int args) /*const*/
+		{
+			return invoke_best_match<PolicyList, ArgList>(L, *this, ctx, f, args);
+		}
+
+		int format_signature(lua_State* L, char const* function, bool concat = true) const
+		{
+			return detail::format_signature(L, function, ArgList(), concat);
+		}
+
+		static std::tuple<bool, int> safe_entry_point(lua_State* L)
+		{
+			void* ud = lua_touserdata(L, lua_upvalueindex(1));
+			function_object_impl* impl = *static_cast<function_object_impl**>(ud);
+			int results = 0;
+			bool error = false;
 #ifndef LUABIND_NO_EXCEPTIONS
-				catch (...)
+			try
+#endif
+			{
+				invoke_context ctx;
+				results = invoke<PolicyList, ArgList>(L, *impl, ctx, impl->f);
+				if (!get_permissive_mode() && !ctx)
 				{
 					error = true;
-					handle_exception_aux(L);
+					ctx.format_error(L, impl);
 				}
-#endif
-				if (error)
-					assert(results >= 0);
-				return {error, results};
 			}
-
-			static int entry_point(lua_State* L)
+#ifndef LUABIND_NO_EXCEPTIONS
+			catch (...)
 			{
-				auto [error, results] = safe_entry_point(L);
-				if (error)
-					return lua_error(L);
-				if (results < 0)
-					return lua_yield(L, -results - 1);
-				return results;
+				error = true;
+				handle_exception_aux(L);
 			}
+#endif
+			if (error)
+				assert(results >= 0);
+			return {error, results};
+		}
 
-			F f;
-		};
+		static int entry_point(lua_State* L)
+		{
+			auto [error, results] = safe_entry_point(L);
+			if (error)
+				return lua_error(L);
+			if (results < 0)
+				return lua_yield(L, -results - 1);
+			return results;
+		}
+
+		F f;
+	};
 
 # ifdef _MSC_VER
 #  pragma pack(pop)
 # endif
 
-		LUABIND_API object make_function_aux(lua_State* L, function_object* impl, bool default_scope = false);
-		LUABIND_API void add_overload(object const&, char const*, object const&);
+	LUABIND_API object make_function_aux(lua_State* L, function_object* impl, bool default_scope = false);
+	LUABIND_API void add_overload(object const&, char const*, object const&);
+} // namespace luabind::detail
 
-	} // namespace detail
-
-	template <class F, typename... SignatureElements, typename... PolicyInjectors >
-	object make_function(lua_State* L, F f, bool default_scope, meta::type_list< SignatureElements... >, meta::type_list< PolicyInjectors... >)
+namespace luabind
+{
+	template <class F, typename... Args, typename... Policies>
+	object make_function(lua_State* L, F f, bool default_scope,
+		meta::type_list<Args...>, meta::type_list<Policies...>)
 	{
-		return detail::make_function_aux(L, luabind_new<detail::function_object_impl<F, meta::type_list< SignatureElements... >, meta::type_list< PolicyInjectors...>>>(f), default_scope);
+		using ArgList = meta::type_list<Args...>;
+		using PolicyList = meta::type_list<Policies...>;
+		auto impl = luabind_new<detail::function_object_impl<F, ArgList, PolicyList>>(f);
+		return detail::make_function_aux(L, impl, default_scope);
 	}
 
-	template <class F, typename... PolicyInjectors >
-	object make_function(lua_State* L, F f, bool default_scope, meta::type_list< PolicyInjectors... >)
+	template <class F, typename... Policies >
+	object make_function(lua_State* L, F f, bool default_scope, meta::type_list<Policies...>)
 	{
-		return make_function(L, f, default_scope, deduce_signature_t<F>(), meta::type_list< PolicyInjectors... >());
+		using PolicyList = meta::type_list<Policies...>;
+		return make_function(L, f, default_scope, deduce_signature_t<F>(), PolicyList());
 	}
 
 	template <class F>
@@ -112,5 +115,4 @@ namespace luabind {
 	{
 		return make_function(L, f, default_scope, deduce_signature_t<F>(), no_policies());
 	}
-
 } // namespace luabind
