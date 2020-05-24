1e733a12d0311253b4b44037a3e8cfd3f52c5f49
Pavel Kovalenko <su@nitrocaster.me>
2020-05-24 19:45:32 +0300

    inheritance.cpp: Refactor & reformat.

--- src/inheritance.cpp
+++ src/inheritance.cpp
@@ -7,229 +7,172 @@
 #include <luabind/typeid.hpp>
 #include <luabind/detail/inheritance.hpp>
 
-namespace luabind {
-	namespace detail {
+namespace luabind::detail
+{
+	char class_id_map_tag;
+	char class_map_tag;
+	char cast_graph_tag;
 
-		char class_id_map_tag = 0;
-		char class_map_tag = 0;
-		char cast_graph_tag = 0;
+	class_id const class_id_map::local_id_base = std::numeric_limits<class_id>::max() / 2;
 
-		class_id const class_id_map::local_id_base = std::numeric_limits<class_id>::max() / 2;
+	struct edge
+	{
+		edge(class_id target, cast_function cast) :
+			target(target),
+			cast(cast)
+		{}
 
-		namespace
+		class_id target;
+		cast_function cast;
+	};
+
+	static bool operator<(edge const& x, edge const& y)
+	{ return x.target < y.target; }
+
+	struct vertex
+	{
+		vertex(class_id id) :
+			id(id)
+		{}
+
+		class_id id;
+		luabind::vector<edge> edges;
+	};
+
+	using cache_entry = std::pair<ptrdiff_t, int>;
+
+	class cache
+	{
+	public:
+		static constexpr ptrdiff_t unknown = std::numeric_limits<ptrdiff_t>::max();
+		static constexpr ptrdiff_t invalid = unknown - 1;
+
+		cache_entry get(class_id src, class_id tgt, class_id d_id, ptrdiff_t obj_offset) const
 		{
+			auto i = m_cache.find(key_type(src, tgt, d_id, obj_offset));
+			return i != m_cache.end() ? i->second : cache_entry(unknown, -1);
+		}
 
-			struct edge
-			{
-				edge(class_id target, cast_function cast)
-					: target(target), cast(cast)
-				{}
-
-				class_id target;
-				cast_function cast;
-			};
-
-			bool operator<(edge const& x, edge const& y)
-			{
-				return x.target < y.target;
-			}
-
-			struct vertex
-			{
-				vertex(class_id id)
-					: id(id)
-				{}
-
-				class_id id;
-				luabind::vector<edge> edges;
-			};
-
-			using cache_entry = std::pair<std::ptrdiff_t, int>;
-
-			class cache
-			{
-			public:
-				static constexpr std::ptrdiff_t unknown = std::numeric_limits<std::ptrdiff_t>::max();
-				static constexpr std::ptrdiff_t invalid = unknown - 1;
-
-				cache_entry get(class_id src, class_id target, class_id dynamic_id, std::ptrdiff_t object_offset) const;
-
-				void put(class_id src, class_id target, class_id dynamic_id, std::ptrdiff_t object_offset, std::ptrdiff_t offset, int distance);
-				void invalidate();
-
-			private:
-				using key_type = std::tuple<class_id, class_id, class_id, std::ptrdiff_t>;
-				using map_type = luabind::map<key_type, cache_entry>;
-				map_type m_cache;
-			};
-
-			constexpr std::ptrdiff_t cache::unknown;
-			constexpr std::ptrdiff_t cache::invalid;
-
-			cache_entry cache::get(class_id src, class_id target, class_id dynamic_id, std::ptrdiff_t object_offset) const
-			{
-				map_type::const_iterator i = m_cache.find(
-					key_type(src, target, dynamic_id, object_offset));
-				return i != m_cache.end() ? i->second : cache_entry(unknown, -1);
-			}
-
-			void cache::put(class_id src, class_id target, class_id dynamic_id, std::ptrdiff_t object_offset, std::ptrdiff_t offset, int distance)
-			{
-				m_cache.emplace(key_type(src, target, dynamic_id, object_offset), cache_entry(offset, distance));
-			}
-
-			void cache::invalidate()
-			{
-				m_cache.clear();
-			}
-
-		} // namespace anonymous
-
-		class cast_graph::impl
+		void put(class_id src, class_id tgt, class_id d_id, ptrdiff_t obj_offset, ptrdiff_t offset, int distance)
 		{
-		public:
-			std::pair<void*, int> cast(
-				void* p, class_id src, class_id target
-				, class_id dynamic_id, void const* dynamic_ptr) const;
-			void insert(class_id src, class_id target, cast_function cast);
+			m_cache.emplace(key_type(src, tgt, d_id, obj_offset), cache_entry(offset, distance));
+		}
 
-		private:
-			luabind::vector<vertex> m_vertices;
-			mutable cache m_cache;
-		};
+		void invalidate()
+		{ m_cache.clear(); }
 
-		namespace
+	private:
+		using key_type = std::tuple<class_id, class_id, class_id, ptrdiff_t>;
+		luabind::map<key_type, cache_entry> m_cache;
+	};
+
+	struct queue_entry
+	{
+		queue_entry(void* p, class_id vertex_id, int distance) :
+			p(p),
+			vertex_id(vertex_id),
+			distance(distance)
+		{}
+
+		void* p;
+		class_id vertex_id;
+		int distance;
+	};
+
+	class cast_graph::impl
+	{
+	public:
+		std::pair<void*, int> cast(void* p, class_id src, class_id tgt, class_id d_id, void const* d_ptr) const
 		{
-
-			struct queue_entry
-			{
-				queue_entry(void* p, class_id vertex_id, int distance)
-					: p(p)
-					, vertex_id(vertex_id)
-					, distance(distance)
-				{}
-
-				void* p;
-				class_id vertex_id;
-				int distance;
-			};
-
-		} // namespace anonymous
-
-		std::pair<void*, int> cast_graph::impl::cast(void* const p, class_id src, class_id target, class_id dynamic_id, void const* dynamic_ptr) const
-		{
-			if (src == target)
+			if (src == tgt)
 				return {p, 0};
-			if(src >= m_vertices.size() || target >= m_vertices.size())
+			if (src >= m_vertices.size() || tgt >= m_vertices.size())
 				return {nullptr, -1};
-
-			std::ptrdiff_t const object_offset = (char const*)dynamic_ptr - (char const*)p;
-			cache_entry cached = m_cache.get(src, target, dynamic_id, object_offset);
-
-			if(cached.first != cache::unknown)
+			ptrdiff_t const obj_offset = (char const*)d_ptr - (char const*)p;
+			cache_entry cached = m_cache.get(src, tgt, d_id, obj_offset);
+			if (cached.first != cache::unknown)
 			{
-				if(cached.first == cache::invalid)
+				if (cached.first == cache::invalid)
 					return {nullptr, -1};
 				return {(char*)p + cached.first, cached.second};
 			}
-
 			luabind::queue<queue_entry> q;
 			q.push(queue_entry(p, src, 0));
-
 			// Original source used boost::dynamic_bitset but didn't make use
 			// of its advanced capability of set operations, that's why I think
-			// it's safe to use a luabind::vector<bool> here.
-
+			// it's safe to use a vector<bool> here.
 			luabind::vector<bool> visited(m_vertices.size(), false);
-
-			while(!q.empty())
+			while (!q.empty())
 			{
 				queue_entry const qe = q.front();
 				q.pop();
-
 				visited[qe.vertex_id] = true;
 				vertex const& v = m_vertices[qe.vertex_id];
-
-				if(v.id == target)
+				if (v.id == tgt)
 				{
-					m_cache.put(
-						src, target, dynamic_id, object_offset
-						, (char*)qe.p - (char*)p, qe.distance
-					);
-
+					m_cache.put(src, tgt, d_id, obj_offset, (char*)qe.p - (char*)p, qe.distance);
 					return {qe.p, qe.distance};
 				}
-
-				for(auto const& e : v.edges) {
-					if(visited[e.target])
+				for (auto const& e : v.edges)
+				{
+					if (visited[e.target])
 						continue;
-					if(void* casted = e.cast(qe.p))
+					if (void* casted = e.cast(qe.p))
 						q.push(queue_entry(casted, e.target, qe.distance + 1));
 				}
 			}
-
-			m_cache.put(src, target, dynamic_id, object_offset, cache::invalid, -1);
-
+			m_cache.put(src, tgt, d_id, obj_offset, cache::invalid, -1);
 			return {nullptr, -1};
 		}
 
-		void cast_graph::impl::insert(class_id src, class_id target, cast_function cast)
+		void insert(class_id src, class_id tgt, cast_function cast)
 		{
-			class_id const max_id = std::max(src, target);
-
-			if(max_id >= m_vertices.size())
+			class_id const max_id = std::max(src, tgt);
+			if (max_id >= m_vertices.size())
 			{
 				m_vertices.reserve(max_id + 1);
-				for(class_id i = m_vertices.size(); i < max_id + 1; ++i)
+				for (class_id i = m_vertices.size(); i < max_id + 1; ++i)
 					m_vertices.emplace_back(i);
 			}
-
 			luabind::vector<edge>& edges = m_vertices[src].edges;
-
-			luabind::vector<edge>::iterator i = std::lower_bound(
-				edges.begin(), edges.end(), edge(target, 0)
-			);
-
-			if(i == edges.end() || i->target != target)
+			auto i = std::lower_bound(edges.begin(), edges.end(), edge(tgt, 0));
+			if (i == edges.end() || i->target != tgt)
 			{
-				edges.emplace(i, target, cast);
+				edges.emplace(i, tgt, cast);
 				m_cache.invalidate();
 			}
 		}
 
-		std::pair<void*, int> cast_graph::cast(void* p, class_id src, class_id target, class_id dynamic_id, void const* dynamic_ptr) const
-		{
-			return m_impl->cast(p, src, target, dynamic_id, dynamic_ptr);
-		}
+	private:
+		luabind::vector<vertex> m_vertices;
+		mutable cache m_cache;
+	};
 
-		void cast_graph::insert(class_id src, class_id target, cast_function cast)
-		{
-			m_impl->insert(src, target, cast);
-		}
+	std::pair<void*, int> cast_graph::cast(void* p, class_id src, class_id tgt,
+		class_id d_id, void const* d_ptr) const
+	{
+		return m_impl->cast(p, src, tgt, d_id, d_ptr);
+	}
 
-		cast_graph::cast_graph()
-			: m_impl(luabind_new<impl>())
-		{}
+	void cast_graph::insert(class_id src, class_id tgt, cast_function cast)
+	{ m_impl->insert(src, tgt, cast); }
 
-		cast_graph::~cast_graph()
-		{}
+	cast_graph::cast_graph() :
+		m_impl(luabind_new<impl>())
+	{}
 
-		class_id allocate_class_id(type_id const& cls)
-		{
-			// use plain map here because this function is called by static initializers,
-			// so luabind::allocator is not set yet
-			using map_type = std::map<type_id, class_id>;
+	cast_graph::~cast_graph() {}
 
-			static map_type registered;
-			static class_id id = 0;
-
-			auto [it, inserted] = registered.emplace(cls, id);
-			if (inserted)
-				++id;
-
-			return it->second;
-		}
-
-	} // namespace detail
-} // namespace luabind
-
+	class_id allocate_class_id(type_id const& cls)
+	{
+		// use plain map here because this function is called by static initializers,
+		// so luabind::allocator is not set yet
+		using map_type = std::map<type_id, class_id>;
+		static map_type registered;
+		static class_id id = 0;
+		auto [it, inserted] = registered.emplace(cls, id);
+		if (inserted)
+			id++;
+		return it->second;
+	}
+} // namespace luabind::detail
