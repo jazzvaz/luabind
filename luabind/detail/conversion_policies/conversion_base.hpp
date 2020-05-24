f0690d08b2b3097455b476e350c59054b904db27
Pavel Kovalenko <su@nitrocaster.me>
2020-05-24 14:41:48 +0300

    make_pointee_instance: Collapse overloads into a single function.

--- luabind/detail/conversion_policies/conversion_base.hpp
+++ luabind/detail/conversion_policies/conversion_base.hpp
@@ -10,49 +10,38 @@
 #include <luabind/pointer_traits.hpp>
 #include <luabind/from_stack.hpp>
 
-
-namespace luabind {
-	namespace detail {
-
-		// Something's strange with the references here... need to know when to copy :(
-		template <class T, class Clone>
-		void make_pointee_instance(lua_State* L, T&& x, std::true_type, Clone)
+namespace luabind::detail
+{
+	template <class T, class Clone>
+	void make_pointee_instance(lua_State* L, T&& x, Clone)
+	{
+		if constexpr (has_get_pointer_v<T>)
 		{
-			if(get_pointer(x))
-			{
+			// Something's strange with the references here... need to know when to copy :(
+			if (get_pointer(x))
 				make_pointer_instance(L, std::forward<T>(x));
+			else
+				lua_pushnil(L);
+		}
+		else
+		{
+			if constexpr (Clone::value)
+			{
+				using value_type = std::remove_reference_t<T>;
+				luabind::unique_ptr<value_type> ptr(luabind_new<value_type>(std::move(x)));
+				make_pointer_instance(L, std::move(ptr));
 			}
 			else
 			{
-				lua_pushnil(L);
+				// need a second make_instance that moves x into place
+				make_pointer_instance(L, &x);
 			}
 		}
-
-		template <class T>
-		void make_pointee_instance(lua_State* L, T&& x, std::false_type, std::true_type)
-		{
-			using value_type = std::remove_reference_t<T>;
-
-			luabind::unique_ptr<value_type> ptr(luabind_new<value_type>(std::move(x)));
-			make_pointer_instance(L, std::move(ptr));
-		}
-
-		template <class T>
-		void make_pointee_instance(lua_State* L, T&& x, std::false_type, std::false_type)
-		{
-			// need a second make_instance that moves x into place
-			make_pointer_instance(L, &x);
-		}
-
-		template <class T, class Clone>
-		void make_pointee_instance(lua_State* L, T&& x, Clone)
-		{
-			make_pointee_instance(L, std::forward<T>(x), has_get_pointer<T>(), Clone());
-		}
-
 	}
+} // namespace luabind::detail
 
+namespace luabind
+{
 	template <class T, class Enable>
 	struct default_converter;
-
-}
+} // namespace luabind
